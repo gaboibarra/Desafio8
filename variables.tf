b67fa650482bb63da35ed238d7c58bd084dfeb4e
gaboibarra <78750985+gaboibarra@users.noreply.github.com>
2025-03-29 15:22:27 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "Región de AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Nombre del bucket S3"
+  type        = string
+}
